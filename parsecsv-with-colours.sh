4f5c9222e190d0e38bbe35222d56371b56ad5f8f
Madhu Reddy <madhu@scientiamobile.com>
2021-07-16 12:19:17 -0400

    .

--- parsecsv-with-colours.sh
+++ parsecsv-with-colours.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
 OLDIFS="$IFS" 
-IFS=$"\n" 
+IFS=$"," 
 while read product price quantity 
 do 
 echo -e "\033[1;33m$product \
-=========================\033[0m\n 
+=========================\033[0m\n  
 Price : \t $price \n 
 Quantity : \t $quantity \n" 
 
